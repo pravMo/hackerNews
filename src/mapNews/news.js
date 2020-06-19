7a85b6f1193d837b3ee22484e63b10a96f469f1f
Prav <thunderousmoneyman@gmail.com>
2020-06-19 10:22:26 -0400

    conditional statements placed out of return function

--- src/mapNews/news.js
+++ src/mapNews/news.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './news.css'
 
 const news = (props) => {
+    const story = props.sText == null ? "No Story" : props.sText
+    const comment = props.cText == null ? "No Comment" : props.cText
+    const date = props.date.split('T')[0]
     return (
     <p className='p'>
         <img className='img' src="/ws.png" alt='Warning Sign' /> 
         <li> TITLE: {props.title} </li>
-        <li> DATE: {props.date.split('T')[0]} </li>
+        <li> DATE: {date} </li>
         <li> AUTHOR: {props.author} </li>
         <li> POINTS: {props.points} </li>
-        <li> STORY: {props.sText === null ? "No Story" : props.sText} </li>
-        <li> COMMENT: {props.cText === null ? "No Comment" : props.cText} </li>
+        <li> STORY: {story} </li>
+        <li> COMMENT: {comment} </li>
         <li> VIEW LINK: <a href={props.url} target="_blank" rel="noopener noreferrer"> {props.url} </a> </li>
     </p>
     )
