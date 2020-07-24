990baad504d2c3b43e6092c2bb128b421582f594
pravMo <67081340+pravMo@users.noreply.github.com>
2020-07-24 12:58:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
   }
 
   onInput = (event) => {
-    const {query} = event.target.value
+    const query = event.target.value
     const {hits} = this.state
    
     if (query.length > 0) {
@@ -35,7 +35,7 @@ class App extends React.Component {
     })
 
         this.setState({
-          query: query, 
+          query: event.target.value, 
           filteredApi: matches, 
         })
 
